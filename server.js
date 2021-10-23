ea4e27651f420be4127f927ba02a95e1a5c65a10
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-10-23 11:08:46 -0400

    created get routes that will 1) take to main index page on invalid address and 2) take you to the notes page when requested

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const { application } = require('express');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const { notes } = require('./db/db.json');
 
-app.get('/db/notes', (req, res) => {
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/notes', (req, res) => {
     res.json(notes);
     console.log(notes);
 });
+
+app.get('/notes', (req, res)=> {
+    res.sendFile('notes.html')
+})
+
+app.get('*', (req, res)=> {
+    res.sendFile('index.html')
+})
 
 app.listen(3001, () => {
     console.log(`API server now on PORT 3001!`);
